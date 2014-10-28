d45e5836e5354fa546e732e02b14c6ea541fce47
Hassan Rasheed <hassan.rasheed@fit.fraunhofer.de>
2014-10-28 17:10:59 +0100

    test added for service catalog client

---
+++ catalog-sdk/service-catalog-client/src/test/java/eu/linksmart/lc/sc/client/ServiceCatalogClientTest.java
@@ -0,0 +1,63 @@
+package eu.linksmart.lc.sc.client;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class ServiceCatalogClientTest {
+	
+	private String BASE_URL = "http://gando.fit.fraunhofer.de:8091";
+	
+	@Test
+	public void testCatalogClient() {
+		
+		String deviceJson = readFileContents("/registration.json");
+		
+		assertTrue(ServiceCatalogClient.getInstance(BASE_URL).registerService(deviceJson));
+		
+		String result_gc = ServiceCatalogClient.getInstance(BASE_URL).getService("sc/testserver/broker");
+		assertNotNull(result_gc);
+		System.out.println("get-service: " + result_gc);
+		
+		String result_sd = ServiceCatalogClient.getInstance(BASE_URL).search("service", "name", "equals", "MqttBroker");
+		assertNotNull(result_sd);
+		System.out.println("search-service: " + result_sd);
+		
+		String updatedServiceJson = readFileContents("/update-registration.json");
+		
+		assertTrue(ServiceCatalogClient.getInstance(BASE_URL).updateService("sc/testserver/broker", updatedServiceJson));
+		
+		String result_updated_gs = ServiceCatalogClient.getInstance(BASE_URL).getService("sc/testserver/broker-2");
+		assertNotNull(result_updated_gs);
+		System.out.println("get-updated_service: " + result_updated_gs);
+		
+		assertTrue(ServiceCatalogClient.getInstance(BASE_URL).deleteService("sc/testserver/broker-2"));
+		
+	}
+	
+	private String readFileContents(String fileName) {
+		
+		File jsonDataFile = new File((this.getClass().getResource((fileName)).getFile()));
+		
+		StringBuilder fileContents = null;
+		Scanner scanner = null;
+	
+		try {
+			fileContents = new StringBuilder((int)jsonDataFile.length());
+			scanner = new Scanner(jsonDataFile);
+			String lineSeparator = System.getProperty("line.separator");
+	        while(scanner.hasNextLine()) {        
+	            fileContents.append(scanner.nextLine() + lineSeparator);
+	        }
+	    } catch(Exception e) {
+	    	e.printStackTrace();
+	    } finally {
+	        scanner.close();
+	    }
+		return fileContents.toString();
+	}
+	
+}
