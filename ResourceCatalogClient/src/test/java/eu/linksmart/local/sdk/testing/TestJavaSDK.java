0f35c925728513e53679d8e1d806b5f897f14bfc
carlos <karl.catewicz@fit.fraunhofer.de>
2014-10-31 18:45:20 +0100

    - simplified tests - crap removed

--- ResourceCatalogClient/src/test/java/eu/linksmart/local/sdk/testing/TestJavaSDK.java
+++ ResourceCatalogClient/src/test/java/eu/linksmart/local/sdk/testing/TestJavaSDK.java
@@ -10,14 +10,11 @@ import org.junit.Test;
 
 import javax.mail.internet.ContentType;
 import javax.mail.internet.ParseException;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import static junit.framework.Assert.*;
 
@@ -27,21 +24,10 @@ import static junit.framework.Assert.*;
 public class TestJavaSDK {
 
     public LSLCDevice device;
-    public String deviceID;
     DeviceManagementClient client;
 
     @Before
     public void setupDeviceRegistrationDocument() throws MalformedURLException, ParseException {
-        String hostID = "localhost";
-
-        try {
-            hostID = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-        deviceID = hostID+"/DeviceA";
-        UUID uuid = UUID.randomUUID();
 
         ContentType aContentType = new ContentType("text/plain");
 
@@ -50,7 +36,6 @@ public class TestJavaSDK {
         device.description = new String("Device registered from Java DeviceManagementClient");
         device.ttl = 60;
 
-
         LSLCProtocol protocol = new LSLCProtocol();
         LSLCRepresentation representation = new LSLCRepresentation();
         LSLCResource resource = new LSLCResource();
@@ -58,7 +43,6 @@ public class TestJavaSDK {
         representation.contentType = aContentType;
         representation.type = new String("number");
 
-        //protocol.endpoint = new URL("http://device_a");
         protocol.endpoint.put("fancy-alien-broker-from-hell", "320uff0:::////30402045r???");
         protocol.endpoint.put("another-void-alltypes-possible-broker-from-hell","???:::/SH-G/¿X");
         protocol.type = new String("REST");
@@ -96,6 +80,7 @@ public class TestJavaSDK {
         System.out.println("[INTEGRATION] device updated.");
 
         assertTrue("could not remove ", client.delete(regID));
+        System.out.println("[INTEGRATION] device removed");
         System.out.println("[INTEGRATION] ****************************************");
 
 
@@ -116,6 +101,7 @@ public class TestJavaSDK {
 
         // test if all fields were retrieved properly
         assertTrue("returned device not equal to original one", isRetrivedInstanceIdentical(deviceFromRC,regID));
+        System.out.println("[INTEGRATION] device removed");
         System.out.println("[INTEGRATION] ****************************************");
 
 
@@ -130,11 +116,12 @@ public class TestJavaSDK {
 
         device.name = "TESTING-GETALLDEVICES";
         String regID = client.add(device);
-        System.out.println("registered device id: "+regID);
+        System.out.println("[INTEGRATION] registered device id: "+regID);
 
         List<LSLCDevice> devices = client.getAllDevices();
 
         client.delete(regID);
+        System.out.println("[INTEGRATION] device removed");
 
         // TODO look up for "my test" device. since the RC changed the ID a workaround is needed. fuuuUU
         for(int i=0; i < devices.size();i++){
@@ -156,62 +143,64 @@ public class TestJavaSDK {
 
         // compare simple values
 
-        // TODO // device id changes due action of RC , BUG . equality test disabled !
-        //if(!retrievedDevice.id.equalsIgnoreCase(device.id)) return false;
+        System.out.println("[INTEGRATION] comparing devices...");
+        assertEquals(expectedID,retrievedDevice.id);
         assertEquals(device.name,retrievedDevice.name);
         assertEquals(device.description,retrievedDevice.description);
         assertEquals(device.ttl,retrievedDevice.ttl);
+        System.out.println("[INTEGRATION] comparing devices OK");
 
         // compare resources of device
-        System.out.println("comparing "+retrievedDevice.resources.size()+" resources");
+        System.out.println("[INTEGRATION] comparing "+retrievedDevice.resources.size()+" resources...");
         for(int i=0; i < retrievedDevice.resources.size(); i++) {
             // compare simple types
-            // TODO // device id changes due action of RC , BUG . equality test disabled !
-            //assertEquals(retrievedDevice.resources.get(i).id,device.resources.get(i).id);
-//            if(!retrievedDevice.resources.get(i).id.equalsIgnoreCase(device.resources.get(i).id)) return false;
             assertEquals(retrievedDevice.resources.get(i).name, device.resources.get(i).name);
-//            if(!retrievedDevice.resources.get(i).name.equalsIgnoreCase(device.resources.get(i).name)) return false;
 
             // compare representation
             LSLCRepresentation retrievedRepresentation = retrievedDevice.resources.get(i).representation;
             assertEquals(retrievedRepresentation.type, device.resources.get(i).representation.type);
             assertEquals(retrievedRepresentation.contentType.toString(),device.resources.get(i).representation.contentType.toString());
 
-            System.out.println("[INTEGRATION] comparing protocols...");
+            System.out.println("  [INTEGRATION] comparing protocols...");
             // compare protocols
             List<LSLCProtocol> retrievedProtocols = retrievedDevice.resources.get(i).protocols;
-            System.out.println("[INTEGRATION] number of protocols: "+retrievedDevice.resources.get(i).protocols.size());
+            System.out.println("  [INTEGRATION] number of protocols: "+retrievedDevice.resources.get(i).protocols.size());
             for(int j=0; j < retrievedProtocols.size();j++){
-                System.out.println("[INTEGRATION] protocol #"+j);
+                System.out.println("  [INTEGRATION] protocol #"+j);
                 LSLCProtocol retrievedProtocol = retrievedProtocols.get(j);
                 // simple types
                 assertEquals(retrievedProtocol.type,device.resources.get(i).protocols.get(j).type);
 
                 // compare alien protocol endpoints
+                System.out.println("    [INTEGRATION] comparing alien endpoints...");
                 HashMap<String, String> alienEndpoints = retrievedProtocol.endpoint;
-                System.out.println("[INTEGRATION] alien endpoints size: "+alienEndpoints.size());
+                System.out.println("      [INTEGRATION] alien endpoints size: "+alienEndpoints.size());
                 HashMap<String, String> originalEndpoints = device.resources.get(i).protocols.get(j).endpoint;
-                System.out.println("[INTEGRATION] original endpoints size: "+originalEndpoints.size());
+                System.out.println("      [INTEGRATION] original endpoints size: "+originalEndpoints.size());
                 for(Map.Entry<String, String> entry : alienEndpoints.entrySet()) {
-                    System.out.println("[INTEGRATION] fancy alien broker endpoint >> "+entry.getKey()+": "+entry.getValue());
+                    System.out.println("      [INTEGRATION] fancy alien broker endpoint >> "+entry.getKey()+": "+entry.getValue());
                     assertEquals(originalEndpoints.get(entry.getKey()), entry.getValue());
                 }
+                System.out.println("    [INTEGRATION] alien endpoints OK");
                 assertEquals(retrievedProtocol.endpoint.toString(), device.resources.get(i).protocols.get(j).endpoint.toString());
                 // protocol methods
-                System.out.println("[INTEGRATION] comparing methods...");
+                System.out.println("    [INTEGRATION] comparing methods...");
                 List<String> retrievedMethods = retrievedProtocol.methods;
                 for(int z=0; z < retrievedMethods.size();z++){
                     assertEquals(retrievedMethods.get(z),device.resources.get(i).protocols.get(j).methods.get(z));
                 }
+                System.out.println("    [INTEGRATION] methods OK");
                 // content-types
                 List<ContentType> retrievedContentTypes = retrievedProtocol.contentTypes;
                 for(int z=0; z < retrievedContentTypes.size();z++){
                     assertEquals(retrievedContentTypes.get(z).toString(),device.resources.get(i).protocols.get(j).contentTypes.get(z).toString());
                 }
             }
+            System.out.println("  [INTEGRATION] protocols OK");
 
 
         }
+        System.out.println("[INTEGRATION] comparing resources OK");
 
         return true;
     }
